2ed4f1942935f6d82b4e91c750ca1ec096249779
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2025-01-24 19:10:06 +0700

    Migrating openportal marketplace plugin to new way of doing things

--- src/waldur_mastermind/marketplace_openportal/apps.py
+++ src/waldur_mastermind/marketplace_openportal/apps.py
@@ -2,36 +2,36 @@ from django.apps import AppConfig
 from django.db.models import signals
 
 
-class MarketplaceSlurmConfig(AppConfig):
-    name = "waldur_mastermind.marketplace_slurm_remote"
-    verbose_name = "Marketplace SLURM Remote"
-    service_name = "SLURM remote"
+class MarketplaceOpenPortalConfig(AppConfig):
+    name = "waldur_mastermind.marketplace_openportal"
+    verbose_name = "Marketplace OpenPortal"
+    service_name = "OpenPortal"
 
     def ready(self):
         from waldur_core.permissions import signals as permission_signals
         from waldur_mastermind.marketplace import handlers as marketplace_handlers
         from waldur_mastermind.marketplace import models as marketplace_models
         from waldur_mastermind.marketplace.plugins import manager
-        from waldur_mastermind.marketplace_slurm_remote import (
+        from waldur_mastermind.marketplace_openportal import (
             PLUGIN_NAME,
             handlers,
             processor,
         )
-        from waldur_mastermind.marketplace_slurm_remote import (
-            registrators as slurm_registrators,
+        from waldur_mastermind.marketplace_openportal import (
+            registrators as openportal_registrators,
         )
-        from waldur_slurm import models as slurm_models
+        from waldur_openportal import models as openportal_models
 
-        slurm_registrators.RemoteSlurmRegistrator.connect()
+        openportal_registrators.OpenPortalRegistrator.connect()
 
         signals.post_save.connect(
             handlers.update_component_quota,
-            sender=slurm_models.Allocation,
-            dispatch_uid="waldur_mastermind.marketplace_slurm.update_component_quota",
+            sender=openportal_models.Allocation,
+            dispatch_uid="waldur_mastermind.marketplace_openportal.update_component_quota",
         )
 
-        marketplace_handlers.connect_resource_handlers(slurm_models.Allocation)
-        marketplace_handlers.connect_resource_metadata_handlers(slurm_models.Allocation)
+        marketplace_handlers.connect_resource_handlers(openportal_models.Allocation)
+        marketplace_handlers.connect_resource_metadata_handlers(openportal_models.Allocation)
 
         manager.register(
             PLUGIN_NAME,
@@ -44,28 +44,28 @@ class MarketplaceSlurmConfig(AppConfig):
 
         signals.post_save.connect(
             handlers.sync_component_user_usage_when_allocation_user_usage_is_submitted,
-            sender=slurm_models.AllocationUserUsage,
-            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.sync_component_user_usage_when_allocation_user_usage_is_submitted",
+            sender=openportal_models.AllocationUserUsage,
+            dispatch_uid="waldur_mastermind.marketplace_openportal.sync_component_user_usage_when_allocation_user_usage_is_submitted",
         )
 
         signals.post_save.connect(
             handlers.send_order_created_to_mqtt,
             sender=marketplace_models.Order,
-            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.send_order_created_to_mqtt",
+            dispatch_uid="waldur_mastermind.marketplace_openportal.send_order_created_to_mqtt",
         )
 
         signals.post_save.connect(
             handlers.send_resource_update_message_to_mqtt,
             sender=marketplace_models.Resource,
-            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.send_resource_status_changed_message_to_mqtt",
+            dispatch_uid="waldur_mastermind.marketplace_openportal.send_resource_status_changed_message_to_mqtt",
         )
 
         permission_signals.role_granted.connect(
             handlers.send_role_granted_message_to_mqtt,
-            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.send_role_granted_message_to_mqtt",
+            dispatch_uid="waldur_mastermind.marketplace_openportal.send_role_granted_message_to_mqtt",
         )
 
         permission_signals.role_revoked.connect(
             handlers.send_role_revoked_message_to_mqtt,
-            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.send_role_revoked_message_to_mqtt",
+            dispatch_uid="waldur_mastermind.marketplace_openportal.send_role_revoked_message_to_mqtt",
         )
---
+++ src/waldur_mastermind/marketplace_openportal/tasks.py
@@ -0,0 +1,76 @@
+import datetime
+
+from celery import shared_task
+from django.utils import timezone
+
+from waldur_core.logging import models as logging_models
+from waldur_mastermind.marketplace import models as marketplace_models
+from waldur_mastermind.marketplace import utils as marketplace_utils
+from waldur_mastermind.marketplace_openportal import PLUGIN_NAME, utils
+
+
+@shared_task(name="waldur_mastermind.marketplace_openportal.sync_offering_users")
+def sync_offering_users():
+    offerings = marketplace_models.Offering.objects.filter(
+        type=PLUGIN_NAME,
+        state__in=[
+            marketplace_models.Offering.States.ACTIVE,
+            marketplace_models.Offering.States.PAUSED,
+        ],
+        secret_options__service_provider_can_create_offering_user=True,
+    ).exclude(
+        plugin_options__username_generation_policy=utils.UsernameGenerationPolicy.SERVICE_PROVIDER.value
+    )
+
+    marketplace_utils.user_offerings_mapping(offerings)
+
+
+@shared_task(
+    name="waldur_mastermind.marketplace_openportal.mark_offering_backend_as_disconnected_after_timeout"
+)
+def mark_offering_backend_as_disconnected_after_timeout():
+    one_hour_ago = timezone.now() - datetime.timedelta(hours=1)
+    integration_statuses = marketplace_models.IntegrationStatus.objects.filter(
+        status=marketplace_models.IntegrationStatus.States.ACTIVE,
+        offering__type=PLUGIN_NAME,
+        last_request_timestamp__lt=one_hour_ago,
+    )
+    for integration_status in integration_statuses:
+        integration_status.set_backend_disconnected()
+        integration_status.save(update_fields=["status"])
+
+
+@shared_task(name="waldur_mastermind.marketplace_openportal.sync_resources")
+def sync_resources():
+    """
+    Sync resources that haven't been updated in the last hour.
+    Only processes resources that users have subscribed to receive updates for.
+    """
+    # Get active subscriptions and their users in a single query
+    active_subscriptions = logging_models.EventSubscription.objects.filter(
+        user__is_active=True, observable_objects__contains=[{"object_type": "resource"}]
+    ).select_related("user")
+
+    # Get all accessible offerings by combining results from each user
+    offering_ids = set()
+    for subscription in active_subscriptions:
+        user_offerings = (
+            marketplace_models.Offering.objects.filter_for_user(subscription.user)
+            .filter(type=PLUGIN_NAME)
+            .values_list("id", flat=True)
+        )
+        offering_ids.update(user_offerings)
+
+    # Get resources that need updating
+    one_hour_ago = timezone.now() - datetime.timedelta(hours=1)
+    resources = (
+        marketplace_models.Resource.objects.filter(
+            offering__id__in=offering_ids, last_sync__lte=one_hour_ago
+        )
+        .select_related("offering")
+        .order_by("last_sync")[:50]
+    )
+
+    # Push updates in bulk
+    for resource in resources:
+        utils.push_resource_update_message(resource)
