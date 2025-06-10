24077d154b6f87e126180f4393c8f205cd883e04
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2025-06-10 12:51:25 +0700

    Separated out the signals

--- src/waldur_openportal/signals.py
+++ src/waldur_openportal/signals.py
@@ -5,3 +5,9 @@ openportal_association_created = Signal()
 
 # providing_args=['allocation', 'user']
 openportal_association_deleted = Signal()
+
+# providing_args=['allocation', 'user']
+openportal_remote_association_created = Signal()
+
+# providing_args=['allocation', 'user']
+openportal_remote_association_deleted = Signal()
